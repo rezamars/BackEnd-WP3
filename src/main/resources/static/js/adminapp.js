b4eb06593f1aac4a927e89850b1201f23db46e34
rezamars <reza.mars@yahoo.com>
2017-02-14 11:45:43 +0100

    Made the specificStationData work.

--- src/main/resources/static/js/adminapp.js
+++ src/main/resources/static/js/adminapp.js
@@ -1,22 +1,39 @@
 
+var kashk ;
+var orgDataList = new Array();
+var indexOf = -1;
+
 var myWeatherApp = angular.module('myWeatherApp', []);
 
 
 myWeatherApp.controller('mainController', function($scope,$http) {
     
+	$scope.open = function (par) {
+		  indexOf = par;
+		  select($scope,$http);
+	}
+	
 	$scope.stationDataList = new Array();
+	$scope.stationorg = new Array();
 	
     $http.get("/weatherDatas").success(function (data) {
     	
     	//alert(data._embedded.weatherDatas[1].stationName);
     	for(var i = 0 ; i<data._embedded.weatherDatas.length ; i++){
     		$scope.stationDataList.push(data._embedded.weatherDatas[i]);
+    		orgDataList.push(data._embedded.weatherDatas[i]);
     	}
     	
-    	
-    	
+    	//orgDataList = $scope.stationDataList;
+    	$scope.stationorg = orgDataList;
+    	//kashk = $scope.stationDataList;
     	$scope.station = removeDub($scope.stationDataList);
-    	//alert($scope.station[0].time);
+    	//$scope.stationorg = orgDataList;
+    	kashk = $scope.stationDataList;
+    	
+    	
+    	
+    	
     }).error(function (status) {
         alert(status);
     });
@@ -40,5 +57,33 @@ function removeDub(array){
 	for(var z = 0 ; z < toRemoveIndexes.length ; z++){
 		newstationDataList.splice(z,1);
 	}
+	
+	newstationDataList.sort(function(a, b) {
+	    return parseFloat(a.smhiID) - parseFloat(b.smhiID);
+	});
+	
 	return newstationDataList;
 }
+
+
+function select($scope,$http) {
+	//angular.element(document.getElementById('mainController')).scope();
+	//alert(indexOf);
+	$scope.specificStationData = new Array();
+	$http.get("/weatherDatas").success(function (data) {
+		
+		for(var i = 0 ; i<data._embedded.weatherDatas.length ; i++){
+			if(data._embedded.weatherDatas[i].stationName == indexOf){
+				$scope.specificStationData.push(data._embedded.weatherDatas[i]);
+				//alert('hej');
+			}
+    	}
+	
+	}).error(function (status) {
+		alert(status);
+	});
+	//alert(specificStationData[0].station.stationName);
+	//alert(document.getElementById('station2-0'));
+}
+
+
